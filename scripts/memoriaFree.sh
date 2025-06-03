b6074e6f7093f7e8243e6624869fb08a813abf0c
Marcelo Portillo <mportillo79@gmail.com>
2025-06-03 10:28:54 -0300

    Se agrega archivo con script de Uso de memoria ram y swap

---
+++ scripts/memoriaFree.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+    echo "----------------------------------------"
+    echo -e "\n USO DE MEMORIA RAM Y SWAP "
+    echo "----------------------------------------"
+    free -h
+    echo ""
