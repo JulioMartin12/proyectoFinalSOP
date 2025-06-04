8b739f99124d1a58f363d7d4263c92ffcc145cd8
Julio Martín <95646667+JulioMartin12@users.noreply.github.com>
2025-06-04 00:37:49 -0300

    Update main_toolkit.sh
    fix el main_tool.sh

--- scripts/main_toolkit.sh
+++ scripts/main_toolkit.sh
@@ -6,18 +6,21 @@ while true; do
   echo "=========================================="
   echo "  Toolkit de Administración del Sistema."
   echo "=========================================="
-  echo "1. Gestión de procesos"
-  echo "2. Salir"
+  echo "1. Gestión de Procesos"
+  echo "2. Gestión de Memoria"
+  echo "3. Gestión de Disco"
+  echo "9. Salir"
   echo "============================="
   read -p "Seleccione una opción: " opcion
 
   case $opcion in
     1) bash procesos.sh ;;
-    2) echo "Saliendo del del Toolkit de Administarción del Sistema ";
-    exit 0 ;;
-    *) echo "Opción inválida.Ingrese una opción correcta 1 | 2.
-     Presione enter para continuar...";
+    2) bash memoria.sh ;;
+    3) bash disco.sh ;;
+    9) echo "Saliendo del del Toolkit de Administarción del Sistema ";
+	     exit 0 ;;
+    *) echo "Opción inválida.Ingrese una opción correcta 1 | 2 | 3.
+	     Presione enter para continuar...";
        read ;;
   esac
 done
-
